8f035d0de97bf7a989aded3f16e25b120fa6f9a2
Gleb Gorelov <gorelov@haulmont.com>
2018-09-04 16:03:46 +0400

    Rework events listeners of UI components with new EventObject approach (1108)
    Fixes

--- modules/web/src/com/haulmont/cuba/web/gui/components/WebPickerField.java
+++ modules/web/src/com/haulmont/cuba/web/gui/components/WebPickerField.java
@@ -44,6 +44,7 @@ import com.haulmont.cuba.web.widgets.CubaPickerField;
 import com.vaadin.data.ValueProvider;
 import com.vaadin.server.Resource;
 import com.vaadin.shared.MouseEventDetails;
+import com.vaadin.shared.Registration;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.InitializingBean;
 
@@ -78,6 +79,7 @@ public class WebPickerField<V extends Entity> extends WebV8AbstractField<CubaPic
     protected List<Action> actions = new ArrayList<>();
     protected Map<String, CubaButton> actionButtons = new HashMap<>();
     protected Map<String, PropertyChangeListener> actionPropertyChangeListeners = new HashMap<>();
+    protected Registration fieldListener;
 
     protected WebPickerFieldActionHandler actionHandler;
 
@@ -377,13 +379,17 @@ public class WebPickerField<V extends Entity> extends WebV8AbstractField<CubaPic
 
     @Override
     public Subscription addFieldValueChangeListener(Consumer<FieldValueChangeEvent<V>> listener) {
-        component.addFieldListener(vEvent -> {
-            FieldValueChangeEvent<V> event = new FieldValueChangeEvent<>(WebPickerField.this,
-                    vEvent.getText(), vEvent.getPrevValue());
-            publish(FieldValueChangeEvent.class, event);
-        });
+        if (fieldListener == null) {
+            fieldListener = component.addFieldListener(this::onFieldValueChange);
+        }
 
         return PickerField.super.addFieldValueChangeListener(listener);
+    }
+
+    protected void onFieldValueChange(CubaPickerField.FieldValueChangeEvent <V> e) {
+        FieldValueChangeEvent<V> event = new FieldValueChangeEvent<>(WebPickerField.this,
+                e.getText(), e.getPrevValue());
+        publish(FieldValueChangeEvent.class, event);
     }
 
     @Override

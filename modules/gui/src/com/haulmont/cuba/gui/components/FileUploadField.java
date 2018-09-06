5cd284c51cfd12f6d7918569f27b9d720c410639
Gleb Gorelov <gorelov@haulmont.com>
2018-09-06 14:52:49 +0400

    Java 8 Functional interfaces (363)
    Replace FileUploadField.FileContentProvider with Supplier<InputStream>

--- modules/gui/src/com/haulmont/cuba/gui/components/FileUploadField.java
+++ modules/gui/src/com/haulmont/cuba/gui/components/FileUploadField.java
@@ -24,6 +24,7 @@ import javax.annotation.Nullable;
 import java.io.InputStream;
 import java.util.UUID;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public interface FileUploadField extends UploadField, Field<FileDescriptor>, Component.Focusable, Buffered {
     String NAME = "upload";
@@ -251,13 +252,9 @@ public interface FileUploadField extends UploadField, Field<FileDescriptor>, Com
      *
      * @param contentProvider content provider
      */
-    void setContentProvider(FileContentProvider contentProvider);
+    void setContentProvider(Supplier<InputStream> contentProvider);
     /**
      * @return FileContentProvider which can be used to read data from field
      */
-    FileContentProvider getContentProvider();
-
-    interface FileContentProvider {
-        InputStream provide();
-    }
+    Supplier<InputStream> getContentProvider();
 }

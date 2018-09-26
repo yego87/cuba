5750fa01680f215912a8a93306078a8df5ae7796
Gleb Gorelov <gorelov@haulmont.com>
2018-09-26 19:38:22 +0400

    Replace lambda creation with method references in WebAbstractDataGrid (1304)

--- modules/web/src/com/haulmont/cuba/web/gui/components/WebAbstractDataGrid.java
+++ modules/web/src/com/haulmont/cuba/web/gui/components/WebAbstractDataGrid.java
@@ -17,7 +17,6 @@
 package com.haulmont.cuba.web.gui.components;
 
 import com.google.common.collect.ImmutableMap;
-import com.haulmont.bali.events.EventRouter;
 import com.haulmont.bali.events.Subscription;
 import com.haulmont.bali.util.Dom4j;
 import com.haulmont.bali.util.Preconditions;
@@ -40,7 +39,7 @@ import com.haulmont.cuba.gui.components.sys.ShowInfoAction;
 import com.haulmont.cuba.gui.data.CollectionDatasource;
 import com.haulmont.cuba.gui.data.Datasource;
 import com.haulmont.cuba.gui.data.DsBuilder;
-import com.haulmont.cuba.gui.data.impl.*;
+import com.haulmont.cuba.gui.data.impl.DatasourceImplementation;
 import com.haulmont.cuba.gui.theme.ThemeConstants;
 import com.haulmont.cuba.gui.theme.ThemeConstantsManager;
 import com.haulmont.cuba.web.App;
@@ -3704,169 +3703,6 @@ public abstract class WebAbstractDataGrid<C extends Grid<E> & CubaEnhancedGrid<E
             } else {
                 grid.setWidth(100, Unit.PERCENTAGE);
             }
-        }
-    }
-
-    @Deprecated
-    public class CollectionDsListenersWrapper implements
-            Datasource.ItemChangeListener,
-            Datasource.ItemPropertyChangeListener,
-            Datasource.StateChangeListener,
-            CollectionDatasource.CollectionChangeListener {
-
-        protected WeakItemChangeListener weakItemChangeListener;
-        protected WeakItemPropertyChangeListener weakItemPropertyChangeListener;
-        protected WeakStateChangeListener weakStateChangeListener;
-        protected WeakCollectionChangeListener weakCollectionChangeListener;
-
-        private EventRouter eventRouter;
-
-        /**
-         * Use EventRouter for listeners instead of fields with listeners List.
-         *
-         * @return lazily initialized {@link EventRouter} instance.
-         * @see EventRouter
-         */
-        protected EventRouter getEventRouter() {
-            if (eventRouter == null) {
-                eventRouter = new EventRouter();
-            }
-            return eventRouter;
-        }
-
-        @SuppressWarnings("unchecked")
-        @Override
-        public void collectionChanged(CollectionDatasource.CollectionChangeEvent e) {
-            // #PL-2035, reload selection from ds
-            Set<E> selectedItems = component.getSelectedItems();
-            if (selectedItems == null) {
-                selectedItems = Collections.emptySet();
-            }
-
-            //noinspection unchecked
-            Set<E> newSelection = selectedItems.stream()
-                    .filter(entity -> e.getDs().containsItem(entity.getId()))
-                    .collect(Collectors.toSet());
-
-            if (e.getDs().getState() == Datasource.State.VALID && e.getDs().getItem() != null) {
-                newSelection.add((E) e.getDs().getItem());
-            }
-
-            if (newSelection.isEmpty()) {
-                setSelected((E) null);
-            } else {
-                setSelectedItems(newSelection);
-            }
-
-            for (Action action : getActions()) {
-                action.refreshState();
-            }
-
-            if (getEventRouter().hasListeners(CollectionDatasource.CollectionChangeListener.class)) {
-                getEventRouter().fireEvent(CollectionDatasource.CollectionChangeListener.class,
-                        CollectionDatasource.CollectionChangeListener::collectionChanged, e);
-            }
-        }
-
-        @SuppressWarnings("unchecked")
-        @Override
-        public void itemChanged(Datasource.ItemChangeEvent e) {
-            for (Action action : getActions()) {
-                action.refreshState();
-            }
-
-            if (getEventRouter().hasListeners(Datasource.ItemChangeListener.class)) {
-                getEventRouter().fireEvent(Datasource.ItemChangeListener.class,
-                        Datasource.ItemChangeListener::itemChanged, e);
-            }
-        }
-
-        @SuppressWarnings("unchecked")
-        @Override
-        public void itemPropertyChanged(Datasource.ItemPropertyChangeEvent e) {
-            for (Action action : getActions()) {
-                action.refreshState();
-            }
-
-            if (getEventRouter().hasListeners(Datasource.ItemPropertyChangeListener.class)) {
-                getEventRouter().fireEvent(Datasource.ItemPropertyChangeListener.class,
-                        Datasource.ItemPropertyChangeListener::itemPropertyChanged, e);
-            }
-        }
-
-        @SuppressWarnings("unchecked")
-        @Override
-        public void stateChanged(Datasource.StateChangeEvent e) {
-            for (Action action : getActions()) {
-                action.refreshState();
-            }
-
-            if (getEventRouter().hasListeners(Datasource.StateChangeListener.class)) {
-                getEventRouter().fireEvent(Datasource.StateChangeListener.class,
-                        Datasource.StateChangeListener::stateChanged, e);
-            }
-        }
-
-        public void addCollectionChangeListener(CollectionDatasource.CollectionChangeListener listener) {
-            getEventRouter().addListener(CollectionDatasource.CollectionChangeListener.class, listener);
-        }
-
-        public void removeCollectionChangeListener(CollectionDatasource.CollectionChangeListener listener) {
-            getEventRouter().removeListener(CollectionDatasource.CollectionChangeListener.class, listener);
-        }
-
-        public void addItemChangeListener(Datasource.ItemChangeListener listener) {
-            getEventRouter().addListener(Datasource.ItemChangeListener.class, listener);
-        }
-
-        public void removeItemChangeListener(Datasource.ItemChangeListener listener) {
-            getEventRouter().removeListener(Datasource.ItemChangeListener.class, listener);
-        }
-
-        public void addItemPropertyChangeListener(Datasource.ItemPropertyChangeListener listener) {
-            getEventRouter().addListener(Datasource.ItemPropertyChangeListener.class, listener);
-        }
-
-        public void removeItemPropertyChangeListener(Datasource.ItemPropertyChangeListener listener) {
-            getEventRouter().removeListener(Datasource.ItemPropertyChangeListener.class, listener);
-        }
-
-        public void addStateChangeListener(Datasource.StateChangeListener listener) {
-            getEventRouter().addListener(Datasource.StateChangeListener.class, listener);
-        }
-
-        public void removeStateChangeListener(Datasource.StateChangeListener listener) {
-            getEventRouter().removeListener(Datasource.StateChangeListener.class, listener);
-        }
-
-        @SuppressWarnings("unchecked")
-        public void bind(CollectionDatasource ds) {
-            weakItemChangeListener = new WeakItemChangeListener(ds, this);
-            ds.addItemChangeListener(weakItemChangeListener);
-
-            weakItemPropertyChangeListener = new WeakItemPropertyChangeListener(ds, this);
-            ds.addItemPropertyChangeListener(weakItemPropertyChangeListener);
-
-            weakStateChangeListener = new WeakStateChangeListener(ds, this);
-            ds.addStateChangeListener(weakStateChangeListener);
-
-            weakCollectionChangeListener = new WeakCollectionChangeListener(ds, this);
-            ds.addCollectionChangeListener(weakCollectionChangeListener);
-        }
-
-        @SuppressWarnings("unchecked")
-        public void unbind(CollectionDatasource ds) {
-            ds.removeItemChangeListener(weakItemChangeListener);
-            weakItemChangeListener = null;
-
-            ds.removeItemPropertyChangeListener(weakItemPropertyChangeListener);
-            weakItemPropertyChangeListener = null;
-
-            ds.removeStateChangeListener(weakStateChangeListener);
-            weakStateChangeListener = null;
-
-            ds.removeCollectionChangeListener(weakCollectionChangeListener);
-            weakCollectionChangeListener = null;
         }
     }
 }
